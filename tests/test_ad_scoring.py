dacf1247f4a9390258ee3c70bd1c33797d79d167
Dhanyanayak12 <dhanyanayak53269@gmail.com>
2025-12-12 12:31:31 +0530

    jenkins

---
+++ tests/test_ad_scoring.py
@@ -0,0 +1,9 @@
+# tests/test_ad_scoring.py
+import unittest
+
+class TestDummy(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
